f9aaeec437704cc19393d5ce0d883067c04c367d
Mocan, Cosmin-Raul <cmocan@deloittece.com>
2023-03-27 20:49:30 +0300

    Solved the terraform cloud issue

--- terraform/vms/definitions.tf
+++ terraform/vms/definitions.tf
@@ -1,7 +1,7 @@
 locals {
   proxmox_target_node     = "coolio"
   template_name           = "debian11-template"
-  network_gateway         = "192.168.0.1"
+  network_gateway         = "192.168.1.1"
   network_subnet_range    = "24"
   vm_master_starting_vmid = 500
   vm_worker_starting_vmid = 550
